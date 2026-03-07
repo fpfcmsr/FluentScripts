1a7806b07ea975d288ebe6b096f624f5cdc65052
fpfcmsr <fpf@alumni.cmu.edu>
2026-03-06 21:11:51 -0800

    improve circular pattern creation

--- 2D-savinous-turbine/savinous-turbine.py
+++ 2D-savinous-turbine/savinous-turbine.py
@@ -1,22 +1,30 @@
 import pathlib
 from build123d import *
-from ocp_vscode import show
+from ocp_vscode import show, Camera
+import numpy as np
 
 # --- Parameters ---
 blade_radius = 50.0
-overlap = 10.0
+overlap = 20.0
 blade_thickness = 3.0
 disk_radius = 100.0     # Radius of the circle holding the turbine
-rect_width = 500.0
-rect_height = 250.0
+rect_width = 800.0
+rect_height = 400.0
 
 # --- Body 1: The Inner Turbine Disk ---
 with BuildSketch() as turbine_disk:
     Circle(disk_radius)
     with BuildSketch(mode=Mode.SUBTRACT):
         with BuildLine() as path:
-            CenterArc((blade_radius - overlap / 2, 0), blade_radius, 0, 180)
-            CenterArc((-(blade_radius - overlap / 2), 0), blade_radius, 180, 180)
+            # 3 blades: 60 deg, 180 deg, -60 deg
+            center = (blade_radius - overlap / 2)
+            l1 = CenterArc((-center, 0), blade_radius, 0, 180)
+            with Locations(Location((0,0), -120)):
+                l2 = CenterArc((-center, 0), blade_radius, 0, 180)
+                add(l2)
+            with Locations(Location((0,0), 120)):
+                l3 = CenterArc((-center, 0), blade_radius, 0, 180)
+                add(l3)
         for edge in path.edges():
             make_face(offset(edge, amount=blade_thickness / 2, kind=Kind.INTERSECTION))
 
@@ -38,5 +46,4 @@ show(
     outer_plate.sketch, 
     names=["Turbine Disk", "Outer Plate"],
     colors=["#0078D7", "#7A7A7A"],
-    reset_camera=True
-)
+    reset_camera=Camera.RESET)
